38f1b331daa154336225b31ddd142fca944e66dc
yoelvp <yoelvp73@gmail.com>
2023-12-08 15:33:57 -0500

    add: user authentication controller

---
+++ app/Http/Controllers/Auth/UserAuthenticationController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+
+class UserAuthenticationController extends Controller {
+  public function login() {
+
+  }
+}
