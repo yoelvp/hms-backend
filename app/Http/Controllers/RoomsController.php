96466fd9eede78a4dbf7ca4c25e0bb1c1fab6370
yoelvp <yoelvp73@gmail.com>
2023-12-20 14:49:32 -0500

    modify: getAll() function

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class RoomsController extends Controller
-{
-    //
+class RoomsController extends Controller {
+  public function getAll(Request $request): JsonResponse {
+    $perPage = $request->input('perPage');
+    $page = $request->input('page');
+    $users = User::query()->paginate(perPage: $perPage, page: $page);
+
+    return response()->json($users, Response::HTTP_OK);
+  }
 }
