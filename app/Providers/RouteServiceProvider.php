b113c93d9885162dd9c315a2d9d892db070cff27
yoelvp <yoelvp73@gmail.com>
2023-12-20 14:50:11 -0500

    fix: prefix of the url

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,7 +29,7 @@ class RouteServiceProvider extends ServiceProvider {
 
     $this->routes(function () {
       Route::middleware('api')
-        ->prefix('v1/api')
+        ->prefix('api/v1')
         ->group(base_path('routes/api.php'));
 
       Route::middleware('web')
