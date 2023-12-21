c14d94ffcd9b6e2f7b4c917fd3fb88349d97d90e
yoelvp <yoelvp73@gmail.com>
2023-12-21 16:42:50 -0500

    modify: add strict types and register function

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -1,18 +1,36 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class RoomsController extends Controller {
+  /**
+   * Get all rooms with pagination
+   *
+   * @param Request $request
+   * @return JsonResponse
+   */
   public function getAll(Request $request): JsonResponse {
     $perPage = $request->input('perPage');
     $page = $request->input('page');
     $users = User::query()->paginate(perPage: $perPage, page: $page);
+    dd($users);
 
-    return response()->json($users, Response::HTTP_OK);
+    return response()->json($users, 200);
+  }
+
+  public function register(): JsonResponse {
+    return response()->json([
+      'room' => [
+        'id' => 1,
+        'name' => 'Room name'
+      ],
+      'message' => 'The room create successfully.'
+    ], 200);
   }
 }
