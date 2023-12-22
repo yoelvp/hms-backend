b644965d3a5643af58a5b4d18dd583b4dadd042f
yoelvp <yoelvp73@gmail.com>
2023-12-22 13:07:14 -0500

    modify: add register a new room service

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Http\Requests\RegisterRoomRequest;
+use App\Models\Room;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -18,18 +19,53 @@ class RoomsController extends Controller {
   public function getAll(Request $request): JsonResponse {
     $perPage = $request->input('perPage');
     $page = $request->input('page');
-    $users = User::query()->paginate(perPage: $perPage, page: $page);
-    dd($users);
+    $rooms = Room::query()->paginate(perPage: $perPage, page: $page);
 
-    return response()->json($users, 200);
+    return response()->json($rooms, 200);
   }
 
-  public function register(): JsonResponse {
+  /**
+   * Register a new Room
+   *
+   * @param RegisterRoomRequest $request
+   * @return JsonResponse
+   */
+  public function register(RegisterRoomRequest $request): JsonResponse {
+    $request->validated();
+
+    $room = Room::query()->create($request->all());
+
     return response()->json([
-      'room' => [
-        'id' => 1,
-        'name' => 'Room name'
-      ],
+      'room' => $room,
+      'message' => 'The room create successfully.'
+    ], 200);
+  }
+
+  /**
+   * Register 2 or more rooms
+   *
+   * @param RegisterRoomRequest $request
+   * @return JsonResponse
+   */
+  public function registerMany(Request $request): JsonResponse {
+    $request->validate([
+      '*.room_number' => 'required|string',
+      '*.loor_number' => 'required|string',
+      '*.number_beds' => 'required|string',
+      '*.bathroom_included' => 'required|bool',
+      '*.hot_water' => 'required|bool',
+      '*.tv_included' => 'required|bool',
+      '*.availability' => 'required|bool',
+      '*.type_room' => 'required|string',
+      '*.price_per_day' => 'required|numeric',
+      '*.price_per_hour' => 'required|numeric',
+    ]);
+
+    $rooms = Room::insert($request->all());
+
+    return response()->json([
+      'room' => $request->all(),
+      'created' => $rooms,
       'message' => 'The room create successfully.'
     ], 200);
   }
